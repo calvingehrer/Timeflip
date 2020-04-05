cc1d1a3fcbb2ce99ff9146299f9378bce47dd83e
Anna-Lena <anna-lena.hetzenauer@student.uibk.ac.at>
2020-04-05 15:57:26 +0200

    VacationException and VacationService added

---
+++ src/main/java/at/qe/sepm/skeleton/services/VacationService.java
@@ -0,0 +1,18 @@
+package at.qe.sepm.skeleton.services;
+
+import at.qe.sepm.skeleton.exceptions.VacationException;
+import at.qe.sepm.skeleton.model.User;
+import at.qe.sepm.skeleton.model.Vacation;
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.util.Set;
+
+public interface VacationService {
+
+    @PreAuthorize("@customPermissionEvaluator.hasPermission(MANAGER) or principal.username eq user.username")
+    void addVacation(User user, Vacation vacation) throws VacationException;
+
+    @PreAuthorize("@customPermissionEvaluator.hasPermission(MANAGER) or principal.username eq user.username")
+    Set<Vacation> getVAcationsFromUser(User user);
+
+}
