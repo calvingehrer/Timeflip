e4693c24d8881e665c500a673e8923f3b043c88f
Calvin Gehrer <calvin.gehrer@student.uibk.ac.at>
2020-04-08 17:50:51 +0200

    Added functionality to add Teams with TeamName and Users. Not working correctly yet.

--- src/main/java/at/qe/sepm/skeleton/ui/controllers/AddTeamController.java
+++ src/main/java/at/qe/sepm/skeleton/ui/controllers/AddTeamController.java
@@ -1,10 +1,13 @@
 package at.qe.sepm.skeleton.ui.controllers;
 
 import at.qe.sepm.skeleton.model.Team;
+import at.qe.sepm.skeleton.model.User;
 import at.qe.sepm.skeleton.services.TeamService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+
+import java.util.Set;
 
 
 @Component
@@ -16,6 +19,8 @@ public class AddTeamController {
 
     private Team team = new Team();
 
+    private Set<User> employees;
+
     public void add(){
         teamService.addNewTeam(team);
 
@@ -23,4 +28,11 @@ public class AddTeamController {
     }
 
     public Team getTeam(){return team;}
+
+    public void addEmployees(){
+        this.team.setEmployees(employees);
+    }
+    public Set<User> getEmployees(){
+        return employees;
+    }
 }
