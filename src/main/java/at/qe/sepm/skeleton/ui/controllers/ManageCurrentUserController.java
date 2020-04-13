bc4df30b6f8f8c507ae3ba4cf84f44a611ccec07
Florian Gerhold <florian.gerhold@student.uibk.ac.at>
2020-04-13 18:24:58 +0200

    added functions for password change

--- src/main/java/at/qe/sepm/skeleton/ui/controllers/ManageCurrentUserController.java
+++ src/main/java/at/qe/sepm/skeleton/ui/controllers/ManageCurrentUserController.java
@@ -1,11 +1,13 @@
 package at.qe.sepm.skeleton.ui.controllers;
 
 import at.qe.sepm.skeleton.model.Interval;
+import at.qe.sepm.skeleton.model.PasswordChange;
 import at.qe.sepm.skeleton.model.User;
 import at.qe.sepm.skeleton.services.UserService;
 import at.qe.sepm.skeleton.ui.beans.SessionInfoBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -27,6 +29,13 @@ public class ManageCurrentUserController {
 
     private String intervall;
 
+    private PasswordChange passwords;
+
+
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public String getIntervall() { return intervall; }
 
     public void setIntervall(String intervall) { this.intervall = intervall; }
@@ -43,6 +52,10 @@ public class ManageCurrentUserController {
         this.setCurrentUser(this.userService.loadUser(currentUser.getUsername()));
     }
 
+    public PasswordChange getPasswords() {
+        return passwords;
+    }
+
     @PostConstruct
     public void init() {
         this.setCurrentUser(userService.getAuthenticatedUser());
@@ -56,6 +69,32 @@ public class ManageCurrentUserController {
         String upperQuery = query.toUpperCase();
         return Interval.getAllIntervals().stream().filter(a -> a.contains(upperQuery)).collect(Collectors.toList());
     }
+
+
+    public boolean checkOldPassword(){
+        if(passwordEncoder.encode(passwords.getOldPassword()) == currentUser.getPassword()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public void changePassword(){
+        if(checkOldPassword()){
+            try {
+                this.currentUser.setPassword(passwordEncoder.encode(passwords.getNewPassword()));
+                this.userService.updateUser(currentUser);
+                reloadUser();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        else{
+            System.out.println("Error: Old Password is not correct");
+        }
+    }
+
     /**
      * Saves changed information
      */
