590f27d9cc004b4a57a9f4080ac5dd45aed571f2
Anna-Lena Hetzenauer <anna-lena.hetzenauer@student.uibk.ac.at>
2020-04-13 22:50:05 +0200

    modifications

--- src/main/java/at/qe/sepm/skeleton/services/UserService.java
+++ src/main/java/at/qe/sepm/skeleton/services/UserService.java
@@ -3,6 +3,7 @@ package at.qe.sepm.skeleton.services;
 import at.qe.sepm.skeleton.model.User;
 import at.qe.sepm.skeleton.model.UserRole;
 import at.qe.sepm.skeleton.repositories.UserRepository;
+import org.hibernate.mapping.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -12,7 +13,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import javax.transaction.Transactional;
-import java.util.Collection;
 import java.util.Date;
 
 /**
@@ -132,6 +132,11 @@ public class UserService {
     public User getAuthenticatedUser() {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         return userRepository.findFirstByUsername(auth.getName());
+    }
+
+    @Transactional
+    public User getManagedUser(User user) {
+        return this.userRepository.findFirstByUsername(user.getUsername());
     }
 
     @Transactional
