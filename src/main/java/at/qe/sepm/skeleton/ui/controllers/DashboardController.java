0d620cf5d2fcb3fdb8da8768754b694f18b64e5e
Anna-Lena Hetzenauer <anna-lena.hetzenauer@student.uibk.ac.at>
2020-05-01 11:59:53 +0200

    Anpassungen

--- src/main/java/at/qe/sepm/skeleton/ui/controllers/DashboardController.java
+++ src/main/java/at/qe/sepm/skeleton/ui/controllers/DashboardController.java
@@ -4,6 +4,7 @@ package at.qe.sepm.skeleton.ui.controllers;
 import at.qe.sepm.skeleton.model.Vacation;
 import at.qe.sepm.skeleton.services.*;
 import at.qe.sepm.skeleton.ui.beans.SessionInfoBean;
+import org.primefaces.event.SelectEvent;
 import org.primefaces.model.DefaultScheduleEvent;
 import org.primefaces.model.LazyScheduleModel;
 import org.primefaces.model.ScheduleEvent;
@@ -13,17 +14,20 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
 import javax.annotation.PostConstruct;
+import javax.faces.event.NamedEvent;
 import java.io.Serializable;
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.Collection;
 import java.util.Date;
 
 @Controller
 @Scope("view")
+@NamedEvent
 public class DashboardController implements Serializable {
 
     private static final long serialVersionUID = 2723138783409404659L;
-    private final ScheduleEvent event = new DefaultScheduleEvent();
+    private ScheduleEvent event = new DefaultScheduleEvent();
     @Autowired
     private UserService userService;
     @Autowired
@@ -47,6 +51,22 @@ public class DashboardController implements Serializable {
         this.lazyEventModel = lazyEventModel;
     }
 
+    public ScheduleEvent getEvent() {
+        return event;
+    }
+
+    public void setEvent(ScheduleEvent event) {
+        this.event = event;
+    }
+
+    public ScheduleModel getEventModel() {
+        return eventModel;
+    }
+
+    public void setEventModel(ScheduleModel eventModel) {
+        this.eventModel = eventModel;
+    }
+
     @PostConstruct
     public void init() {
         this.lazyEventModel = new LazyScheduleModel() {
@@ -64,13 +84,33 @@ public class DashboardController implements Serializable {
                 vacations.forEach(f -> {
 
                     Date vacatoionStart = Date.from(f.getStart());
-                    Date vacationEnd = Date.from(f.getEnd());
+                    Date vacationEnd = Date.from(f.getEnd().plus(1, ChronoUnit.MINUTES));
 
-                    addEvent(new DefaultScheduleEvent(f.toString(), vacatoionStart, vacationEnd));
+                    addEvent(new DefaultScheduleEvent("Vacation", vacatoionStart, vacationEnd));
                 });
             }
         };
     }
 
+    public void addEvent() {
+        if (event.isAllDay()) {
+            //see https://github.com/primefaces/primefaces/issues/1164
+            if (event.getStartDate().toInstant().equals(event.getEndDate().toInstant())) {
+                //event.setEndDate(event.getEndDate().plusDays(1));
+            }
+        }
+
+
+        if (event.getId() == null)
+            eventModel.addEvent(event);
+        else
+            eventModel.updateEvent(event);
+
+        event = new DefaultScheduleEvent();
+    }
+
+    public void onEventSelect(SelectEvent selectEvent) {
+        event = (ScheduleEvent) selectEvent.getObject();
+    }
 
 }
