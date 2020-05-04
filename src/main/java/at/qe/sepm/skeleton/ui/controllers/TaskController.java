a0d12c99d1b3a444fe17c3b6ea319e265dedcc56
Anna-Lena Hetzenauer <anna-lena.hetzenauer@student.uibk.ac.at>
2020-05-04 15:17:53 +0200

    task.xhtml erstellt und Anpassungen Controller

--- src/main/java/at/qe/sepm/skeleton/ui/controllers/TaskController.java
+++ src/main/java/at/qe/sepm/skeleton/ui/controllers/TaskController.java
@@ -1,8 +1,8 @@
 package at.qe.sepm.skeleton.ui.controllers;
 
 import at.qe.sepm.skeleton.model.Task;
-import at.qe.sepm.skeleton.model.User;
 import at.qe.sepm.skeleton.services.TaskService;
+import at.qe.sepm.skeleton.ui.beans.SessionInfoBean;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
@@ -15,12 +15,15 @@ public class TaskController {
     @Autowired
     private TaskService taskService;
 
+    @Autowired
+    private SessionInfoBean sessionInfoBean;
+
     public long duration() {
         return taskService.getDuration(this.task);
     }
 
-    public List<Task> getTasksFromUser(User user) {
-        return taskService.getAllTasksFromUser(user);
+    public List<Task> getTasksFromUser() {
+        return taskService.getAllTasksFromUser(sessionInfoBean.getCurrentUser());
     }
 
     public Task getTask() {
@@ -30,4 +33,8 @@ public class TaskController {
     public void setTask(Task task) {
         this.task = task;
     }
+
+
+
+
 }
