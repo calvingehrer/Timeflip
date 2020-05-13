b86c206331834f9a48674b52db2aa83aae9f64cb
Florian Gerhold <florian.gerhold@student.uibk.ac.at>
2020-05-13 17:14:45 +0200

    fixed Department and Team statistic

--- src/main/java/at/qe/sepm/skeleton/repositories/TaskRepository.java
+++ src/main/java/at/qe/sepm/skeleton/repositories/TaskRepository.java
@@ -13,20 +13,16 @@ public interface TaskRepository extends AbstractRepository<Task, String> {
     @Query("SELECT t FROM Task t WHERE t.user=:user")
     List<Task> findTasksFromUser(@Param("user") User user);
 
-
-    @Query("SELECT t as task FROM Task t WHERE t.user=:user and t.startTime>=:start_date and t.endTime <=:end_date")
+    @Query("SELECT t FROM Task t WHERE t.user=:user and t.startTime>=:start_date and t.endTime <=:end_date")
     List<Task> findUserTasksBetweenDates(@Param("user") User user, @Param("start_date") Instant start_date, @Param("end_date") Instant end_date);
 
     @Query("SELECT t FROM Task t WHERE t.team=:team")
     List<Task> findTasksFromTeam(@Param("team") Team team);
 
-    @Query("SELECT t as task FROM Task t WHERE t.team=:team and t.startTime>=:start_date and t.endTime<=:end_date")
+    @Query("SELECT t FROM Task t WHERE t.team=:team and t.startTime>=:start_date and t.endTime<=:end_date")
     List<Task> findTeamTasksBetweenDates(@Param("team") Team team, @Param("start_date") Instant start_date, @Param("end_date") Instant end_date);
 
-    @Query("SELECT t as task FROM Task t WHERE t.team=:department and t.startTime>=:start_date and t.endTime<=:end_date")
+    @Query("SELECT t FROM Task t WHERE t.department=:department and t.startTime>=:start_date and t.endTime<=:end_date")
     List<Task> findDepartmentTasksBetweenDates(@Param("department") Department department, @Param("start_date") Instant start_date, @Param("end_date") Instant end_date);
-
-    @Query("SELECT t as task FROM Task t WHERE t.team=:department and t.startTime>=:start_date and t.endTime<=:end_date")
-    List<Task> findDepartmentTasksBetweenDates2(@Param("department") Department department, @Param("start_date") Instant start_date, @Param("end_date") Instant end_date);
 
 }
