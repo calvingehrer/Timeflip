72c810ff9256e530ceb86a8492e2b01e08258bea
Calvin Gehrer <calvin.gehrer@student.uibk.ac.at>
2020-05-21 12:05:24 +0200

    Get rooms without raspberry is working correct now

--- src/main/java/at/qe/sepm/skeleton/repositories/RoomRepository.java
+++ src/main/java/at/qe/sepm/skeleton/repositories/RoomRepository.java
@@ -9,6 +9,6 @@ public interface RoomRepository extends AbstractRepository<Room, String> {
 
     Room findByRoomNumber(String roomNumber);
 
-    @Query("SELECT r FROM Room r WHERE r.raspberry IS null")
-    List<Room> findRoomsWithoutRaspberry();
+    @Query("SELECT r FROM Room r")
+    List<Room> findAllRooms();
 }
