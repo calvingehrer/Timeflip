1149c7a15c910b8b99cbb37252af792a7437a9d3
Anna-Lena Hetzenauer <anna-lena.hetzenauer@student.uibk.ac.at>
2020-05-23 12:50:15 +0200

    AuditLog Erweiterung für Darstellung, AuditLogListController.java und Anpassung für auditlog.xhtml

--- src/main/java/at/qe/sepm/skeleton/ui/controllers/AuditLogListController.java
+++ src/main/java/at/qe/sepm/skeleton/ui/controllers/AuditLogListController.java
@@ -1,6 +1,5 @@
 package at.qe.sepm.skeleton.ui.controllers;
 
-
 import at.qe.sepm.skeleton.services.AuditLogService;
 import at.qe.sepm.skeleton.utils.auditlog.LogEntry;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,35 +7,26 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
+import java.util.Collection;
 
 @Component
 @Scope("view")
-public class AuditLogDetailController implements Serializable {
+public class AuditLogListController implements Serializable {
 
     @Autowired
     private AuditLogService auditLogService;
 
     private LogEntry logEntry;
 
-    public void setLogEntry(LogEntry logEntry) {
-        this.logEntry = logEntry;
-        //doReloadLogEntry();
-    }
-
-    /**
-     * Returns the currently displayed LogEntry.
-     *
-     * @return
-     */
-    public LogEntry getEntry() {
+    public LogEntry getLogEntry() {
         return logEntry;
     }
 
-    /**
-     * Action to force a reload of the currently displayed room.
-     */
+    public void setLogEntry(LogEntry logEntry) {
+        this.logEntry = logEntry;
+    }
 
-    public void doReloadLogEntry() {
-        // logEntry = auditLogService.getAllEntries();
+    public Collection<LogEntry> getEntries() {
+        return auditLogService.getAllEntries();
     }
 }
