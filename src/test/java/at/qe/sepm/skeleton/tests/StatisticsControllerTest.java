c97ad587ed1545be503c011b46e49d7e34b258e0
Florian Gerhold <florian.gerhold@student.uibk.ac.at>
2020-05-26 01:12:37 +0200

    tried test

---
+++ src/test/java/at/qe/sepm/skeleton/tests/StatisticsControllerTest.java
@@ -0,0 +1,59 @@
+package at.qe.sepm.skeleton.tests;
+
+import at.qe.sepm.skeleton.model.User;
+import at.qe.sepm.skeleton.ui.beans.CurrentUserBean;
+import at.qe.sepm.skeleton.ui.controllers.StatisticsController;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.util.Calendar;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+@WebAppConfiguration
+class StatisticsControllerTest {
+
+    @Autowired
+    CurrentUserBean currentUserBean;
+
+    @DirtiesContext
+    @Test
+    @WithMockUser(username = "admin", authorities = {"ADMIN"})
+    void init() {
+        StatisticsController statisticsController = new StatisticsController();
+
+        Assert.assertNotNull(statisticsController.getTodayUserModel());
+        Assert.assertNull(statisticsController.getChosenDate());
+
+
+    }
+
+    @Test
+    @WithMockUser(username = "admin", authorities = {"ADMIN"})
+    void testRebuildChartsUser() {
+        StatisticsController statisticsController = new StatisticsController();
+
+
+    }
+
+    @Test
+    void rebuildChartsTeam() {
+    }
+
+    @Test
+    void rebuildChartsDepartment() {
+    }
+
+    @Test
+    void setDayToBeginning() {
+    }
+}
